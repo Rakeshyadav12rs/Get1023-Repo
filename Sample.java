1bf5f011cccb1c56b46718bab05171f7a846ca14
Rakeshyadav12rs <109198232+Rakeshyadav12rs@users.noreply.github.com>
2022-07-14 17:21:52 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ class firstprogram
      public static void main(String args[])
      {
            System.out.println("Hello Word");
-          System.out.println("Hi");
+           System.out.println("Hi");
+           System.out.println("Lti");
      }
 }
